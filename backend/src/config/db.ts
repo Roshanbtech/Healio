4bea3be9aad0a3e094768dbf8e71b628cde2792d
Roshan Reji <roshanreji987@gmail.com>
2025-01-26 01:47:59 +0530

    db connection establishment checker.

---
+++ backend/src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        console.log(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL as string);
+        console.log("Hurray! Database connected");
+    } catch (error) {
+        console.log("Database betrayed us");
+    }
+}
+
+export default connectDB;
