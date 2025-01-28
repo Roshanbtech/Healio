ffdbab73a469398c77e9125dd1f896141a2514e3
Roshan Reji <roshanreji987@gmail.com>
2025-01-28 12:06:16 +0530

    some more basic folder addons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+// import UserRoutes from "./src/Routes/userRoutes";
+// import AdminRoutes from "./src/Routes/adminRoutes";
+// import DoctorRoutes from "./src/Routes/doctorRoutes";
+
+
+
 
 const App = () => {
   return (
-    <div className="App text-purple-600 ">App</div>
+  <>
+  <div className="App text-purple-600 underline">App</div>
+  <Router>
+    <Routes>
+      {/* <Route path="/users" element={<UserRoutes />} />
+      <Route path="/doctors" element={<DoctorRoutes />} />
+      <Route path="/admin" element={<AdminRoutes />} /> */}
+    </Routes>
+  </Router>
+  </>
   )
 }
 
-export default App
+export default App;
