c2e74162fd79b2ebc08e793dfd230086286979db
Roshan Reji <roshanreji987@gmail.com>
2025-04-07 02:31:25 +0530

    chatimgbtn disabled

--- frontend/src/components/doctorComponents/Chats.tsx
+++ frontend/src/components/doctorComponents/Chats.tsx
@@ -601,6 +601,7 @@ export default function DoctorChat() {
                       className="hidden"
                       multiple
                       accept="image/*"
+                      disabled
                     />
                     <button
                       disabled
