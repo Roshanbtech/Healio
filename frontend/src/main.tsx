241c780c0bceca351267bc2ad48addd47957e0c0
Roshan Reji <roshanreji987@gmail.com>
2025-04-12 03:03:52 +0530

    buffer addon on main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App.tsx';
 import AppContextProvider from './context/AppContext.tsx';
 import { SocketProvider } from './context/SocketContext.tsx';
+import { Buffer } from 'buffer';
+if (!window.Buffer) window.Buffer = Buffer;
+if (!window.global) window.global = window.globalThis;
 
 
 createRoot(document.getElementById('root')!).render(
